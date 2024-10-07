aafb80902f5ffe07645aeb8de467de01835a7d32
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-07 11:27:38 +0400

    feat: add implementation of function compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
 const compress = async () => {
-    // Write your code here 
+  const source = createReadStream("src/zip/files/fileToCompress.txt");
+  const destination = createWriteStream("src/zip/files/archive.gz");
+  const gzip = createGzip();
+
+  try {
+    await pipeline(source, gzip, destination);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await compress();
